52e3148e557de96bafdf74fa6bb523dbd0f354ce
LuuSean <cuong.luusean@gmail.com>
2019-01-26 21:45:49 +0700

    Install Laravel Passport successfully

--- app/Http/Controllers/API/ClassRoomController.php
+++ app/Http/Controllers/API/ClassRoomController.php
@@ -11,7 +11,7 @@ class ClassRoomController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('api');
+        $this->middleware('auth:api');
     }
     /**
      * Display a listing of the resource.
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,10 @@
                 </section>
                 <ul class="list-unstyled components" id="test">
                     <li class="active">
-                        <router-link to="/">Trang Chủ</>
+                        <router-link to="/">Trang Chủ</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/developer">Developer</router-link>
                     </li>
                     @can('isAdmin')
                     <li>
